e8332bb0012eff891ed5b864085b3baff5f3822e
easykoala <easykoala@126.com>
2022-10-15 11:12:18 +0800

    Add files via upload

---
+++ bundle.js
@@ -0,0 +1,30 @@
+(function (sapper) {
+	'use strict';
+
+	function _interopNamespace(e) {
+		if (e && e.__esModule) return e;
+		var n = Object.create(null);
+		if (e) {
+			Object.keys(e).forEach(function (k) {
+				if (k !== 'default') {
+					var d = Object.getOwnPropertyDescriptor(e, k);
+					Object.defineProperty(n, k, d.get ? d : {
+						enumerable: true,
+						get: function () { return e[k]; }
+					});
+				}
+			});
+		}
+		n["default"] = e;
+		return Object.freeze(n);
+	}
+
+	var sapper__namespace = /*#__PURE__*/_interopNamespace(sapper);
+
+	sapper__namespace.start({
+		target: document.querySelector('#sapper')
+	});
+
+	return sapper__namespace;
+
+})(sapper);
